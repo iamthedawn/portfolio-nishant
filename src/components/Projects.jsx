16ab32fee4fe1f5f162a939d2899318f14e0761c
iamthedawn <nishhantm@gmail.com>
2025-06-08 16:34:33 +0530

    moved projects to config file

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,120 +1,26 @@
 import React from "react";
 import "./Projects.scss";
 import ProjectsCard from "./ProjectsCard";
-import proj1 from "./../../assets/FitIndiaProj.png";
-import ticTacToe from "./../../assets/TicTacToeProj.png";
-import CodeStunt from "./../../assets/CodeStuntProj.png";
-import TinderClone from "./../../assets/TinderCloneProj.png";
-import NetworkTeam from "./../../assets/NetworkProj.png";
-import CodeRunner from "./../../assets/CodeRunnerProj.png";
-import DrumPlay from "./../../assets/DrumPlayProj.png";
-import DiceGame from "./../../assets/DiceGameProj.png";
-import FlowChartBuilder from "./../../assets/FlowChartProj.png";
-import ToDoList from "./../../assets/ToDoListProj.png";
 import "animate.css";
+import { projects } from "../config/config";
 
 const Projects = () => {
   return (
     <div className="projectsContainer">
       <div className="projectsCardList animate__animated animate__slideInRight">
-         <ProjectsCard
-          projName={"FlowChart Builder"}
-          projDesc={
-            "Responsive React app to build, export, and share flowcharts easily."
-          }
-          projUrl={"https://flowchartbuilder.vercel.app/"}
-          projImg={FlowChartBuilder}
-          projUrlTxt={"flowchartbuilder.vercel.app"}
-          projTech={"ReactJS,JS,HTML,CSS,JS"}
-        />
-         <ProjectsCard
-          projName={"Todo List"}
-          projDesc={
-            "Responsive React app to create and save Todo items on the go."
-          }
-          projUrl={"https://iamthedawn.github.io/TodoList/"}
-          projImg={ToDoList}
-          projUrlTxt={"Webpage"}
-          projTech={"ReactJS,JS,HTML,CSS,JS"}
-        />
-        <ProjectsCard
-          projName={"FitIndia Webiste"}
-          projDesc={
-            "A fully responsive multi-page website of Institute’s FIT INDIA Club."
-          }
-          projUrl={"https://fitindiaiiitkota.vercel.app/"}
-          projImg={proj1}
-          projUrlTxt={"fitindiaiiitkota.vercel.app"}
-          projTech={"HTML,CSS,JS"}
-        />
-        <ProjectsCard
-          projName={"TicTacToe Game"}
-          projDesc={
-            "A fun game deployed on a web page which can be played by 2 players."
-          }
-          projUrl={"https://iamthedawn.github.io/tictactoe/"}
-          projImg={ticTacToe}
-          projUrlTxt={"Webpage"}
-          projTech={"HTML,CSS,Bootstrap,JS"}
-        />
-        <ProjectsCard
-          projName={"CodeStunt Website"}
-          projDesc={
-            "A fully stack multi-page website where users can share & find code snippets"
-          }
-          projUrl={"https://kartik0406.github.io/Codestunt/index.html"}
-          projImg={CodeStunt}
-          projUrlTxt={"Webpage"}
-          projTech={"HTML,CSS,JS"}
-        />
-        <ProjectsCard
-          projName={"Tinder Clone"}
-          projDesc={
-            "A landing page website based on tinder theme and resembles tinder.com website."
-          }
-          projUrl={"https://iamthedawn.github.io/tinderclone/"}
-          projImg={TinderClone}
-          projUrlTxt={"Webpage"}
-          projTech={"HTML,CSS,Bootstrap,JS"}
-        />
-        <ProjectsCard
-          projName={"IIITians Network Website"}
-          projDesc={
-            "A landing page website which gives detailed information about members of the community."
-          }
-          projUrl={"https://iamthedawn.github.io/Team-1/index.html"}
-          projImg={NetworkTeam}
-          projUrlTxt={"Webpage"}
-          projTech={"HTML,CSS,JS"}
-        />
-        <ProjectsCard
-          projName={"Interactive Drum Kit"}
-          projDesc={
-            "A WebApp which lets you Tap on drum image to hear its sound — a fun way to explore percussion instruments!"
-          }
-          projUrl={"https://iamthedawn.github.io/drumplay/"}
-          projImg={DrumPlay}
-          projUrlTxt={"WebApp"}
-          projTech={"HTML,CSS,JS"}
-        />
-        <ProjectsCard
-          projName={"Dice Game"}
-          projDesc={"A fun dice game deployed on a web page."}
-          projUrl={"https://iamthedawn.github.io/thedicegame/"}
-          projImg={DiceGame}
-          projUrlTxt={"WebApp"}
-          projTech={"HTML,CSS,JS"}
-        />
-        <ProjectsCard
-          projName={"Your name in ❤️"}
-          projDesc={
-            "A single webpage which prints users name in Heart.It is compiled and run on the web using trinket code runner."
-          }
-          projUrl={"https://iamthedawn.github.io/runner/"}
-          projImg={CodeRunner}
-          projUrlTxt={"Webpage"}
-          projTech={"HTML,CSS,Python"}
-        />
+        {projects?.map((project, idx) => {
+          return (
+            <ProjectsCard
+              key={idx}
+              projName={project?.projName}
+              projDesc={project?.projDesc}
+              projUrl={project?.projUrl}
+              projImg={project?.projImg}
+              projUrlTxt={project?.projUrlTxt}
+              projTech={project?.projTech}
+            />
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/config/config.js
@@ -0,0 +1,105 @@
+import FitIndia from "./../../assets/FitIndiaProj.png";
+import ticTacToe from "./../../assets/TicTacToeProj.png";
+import CodeStunt from "./../../assets/CodeStuntProj.png";
+import TinderClone from "./../../assets/TinderCloneProj.png";
+import NetworkTeam from "./../../assets/NetworkProj.png";
+import CodeRunner from "./../../assets/CodeRunnerProj.png";
+import DrumPlay from "./../../assets/DrumPlayProj.png";
+import DiceGame from "./../../assets/DiceGameProj.png";
+import FlowChartBuilder from "./../../assets/FlowChartProj.png";
+import ToDoList from "./../../assets/ToDoListProj.png";
+
+export const projects = [
+  {
+    projName: "FlowChart Builder",
+    projDesc:
+      "Responsive React app to build, export, and share flowcharts easily.",
+
+    projUrl: "https://flowchartbuilder.vercel.app/",
+    projImg: FlowChartBuilder,
+    projUrlTxt: "flowchartbuilder.vercel.app",
+    projTech: "ReactJS,JS,HTML,CSS,JS",
+  },
+  {
+    projName: "Todo List",
+    projDesc: "Responsive React app to create and save Todo items on the go.",
+
+    projUrl: "https://iamthedawn.github.io/TodoList/",
+    projImg: ToDoList,
+    projUrlTxt: "Webpage",
+    projTech: "ReactJS,JS,HTML,CSS,JS",
+  },
+  {
+    projName: "FitIndia Webiste",
+    projDesc:
+      "A fully responsive multi-page website of Institute’s FIT INDIA Club.",
+
+    projUrl: "https://fitindiaiiitkota.vercel.app/",
+    projImg: FitIndia,
+    projUrlTxt: "fitindiaiiitkota.vercel.app",
+    projTech: "HTML,CSS,JS",
+  },
+  {
+    projName: "TicTacToe Game",
+    projDesc:
+      "A fun game deployed on a web page which can be played by 2 players.",
+    projUrl: "https://iamthedawn.github.io/tictactoe/",
+    projImg: ticTacToe,
+    projUrlTxt: "Webpage",
+    projTech: "HTML,CSS,Bootstrap,JS",
+  },
+  {
+    projName: "CodeStunt Website",
+    projDesc:
+      "A fully stack multi-page website where users can share & find code snippets",
+
+    projUrl: "https://kartik0406.github.io/Codestunt/index.html",
+    projImg: CodeStunt,
+    projUrlTxt: "Webpage",
+    projTech: "HTML,CSS,JS",
+  },
+  {
+    projName: "Tinder Clone",
+    projDesc:
+      "A landing page website based on tinder theme and resembles tinder.com website.",
+    projUrl: "https://iamthedawn.github.io/tinderclone/",
+    projImg: TinderClone,
+    projUrlTxt: "Webpage",
+    projTech: "HTML,CSS,Bootstrap,JS",
+  },
+  {
+    projName: "IIITians Network Website",
+    projDesc:
+      "A landing page website which gives detailed information about members of the community.",
+    projUrl: "https://iamthedawn.github.io/Team-1/index.html",
+    projImg: NetworkTeam,
+    projUrlTxt: "Webpage",
+    projTech: "HTML,CSS,JS",
+  },
+  {
+    projName: "Interactive Drum Kit",
+    projDesc:
+      "A WebApp which lets you Tap on drum image to hear its sound — a fun way to explore percussion instruments!",
+    projUrl: "https://iamthedawn.github.io/drumplay/",
+    projImg: DrumPlay,
+    projUrlTxt: "WebApp",
+    projTech: "HTML,CSS,JS",
+  },
+  {
+    projName: "Dice Game",
+    projDesc: "A fun dice game deployed on a web page.",
+    projUrl: "https://iamthedawn.github.io/thedicegame/",
+    projImg: DiceGame,
+    projUrlTxt: "WebApp",
+    projTech: "HTML,CSS,JS",
+  },
+  {
+    projName: "Your name in ❤️",
+    projDesc:
+      "A single webpage which prints users name in Heart.It is compiled and run on the web using trinket code runner.",
+    projUrl: "https://iamthedawn.github.io/runner/",
+    projImg: CodeRunner,
+    projUrlTxt: "Webpage",
+    projTech: "HTML,CSS,Python",
+  },
+];
