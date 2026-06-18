b9376912dcef9be67110706293ce13b7cb7e5d0c
iamthedawn <nishhantm@gmail.com>
2026-06-18 20:08:17 +0530

    reversed the order keeping the latest exp first

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -22,104 +22,89 @@ const Experience = () => {
       <Helmet>
         <title>Experience | Nishant Mourya</title>
       </Helmet>
+
       <div className="experienceCardList animate__animated animate__slideInRight">
         <div className="experienceCardsContainer experienceCardsContainerFlex">
-          <motion.div className="dragConstraintBox" ref={constraintsRef1}>
+          {/* Latest Experience - Scapia */}
+          <motion.div className="dragConstraintBox" ref={constraintsRef4}>
             <motion.div
               drag
-              dragConstraints={constraintsRef1}
+              dragConstraints={constraintsRef4}
               // dragElastic={0.2}
               // style={{ width: "fit-content" }}
             >
               <ExperienceCards
-                company={"SkillShip Foundation"}
-                role={"Web Development Intern"}
-                duration={"July 2020 - Jan 2021"}
-                logo={internlogo}
-              />
-            </motion.div>
-          </motion.div>
-          <motion.div className="dragConstraintBox" ref={constraintsRef2}>
-            <motion.div
-              drag
-              dragConstraints={constraintsRef2}
-              // dragElastic={0.2}
-              // style={{ width: "fit-content" }}
-            >
-              <ExperienceCards
-                company={"Jio Platforms Limited"}
+                company={"Scapia Cards"}
                 role={"Software Development Engineer"}
-                duration={"July 2023 - Sept 2025"}
-                logo={jiologo}
+                duration={"May 2026 - Present"}
+                logo={scapialogo}
               />
             </motion.div>
           </motion.div>
+
+          {/* eka.care */}
+          <motion.div className="dragConstraintBox" ref={constraintsRef3}>
+            <motion.div drag dragConstraints={constraintsRef3}>
+              <ExperienceCards
+                company={"eka.care"}
+                role={"Software Development Engineer"}
+                duration={"Sept 2025 - May 2026"}
+                logo={ekalogo}
+              />
+            </motion.div>
+          </motion.div>
+
           {isMobile && (
             <>
-              <motion.div className="dragConstraintBox" ref={constraintsRef3}>
-                <motion.div
-                  drag
-                  dragConstraints={constraintsRef3}
-                  // dragElastic={0.2}
-                  // style={{ width: "fit-content" }}
-                >
+              {/* Jio */}
+              <motion.div className="dragConstraintBox" ref={constraintsRef2}>
+                <motion.div drag dragConstraints={constraintsRef2}>
                   <ExperienceCards
-                    company={"eka.care"}
+                    company={"Jio Platforms Limited"}
                     role={"Software Development Engineer"}
-                    duration={"Sept 2025 - May 2026"}
-                    logo={ekalogo}
+                    duration={"July 2023 - Sept 2025"}
+                    logo={jiologo}
                   />
                 </motion.div>
               </motion.div>
 
-              <motion.div className="dragConstraintBox" ref={constraintsRef4}>
-                <motion.div
-                  drag
-                  dragConstraints={constraintsRef4}
-                  // dragElastic={0.2}
-                  // style={{ width: "fit-content" }}
-                >
+              {/* SkillShip */}
+              <motion.div className="dragConstraintBox" ref={constraintsRef1}>
+                <motion.div drag dragConstraints={constraintsRef1}>
                   <ExperienceCards
-                    company={"Scapia Cards"}
-                    role={"Software Development Engineer"}
-                    duration={"June 2026 - Present"}
-                    logo={scapialogo}
+                    company={"SkillShip Foundation"}
+                    role={"Web Development Intern"}
+                    duration={"July 2020 - Jan 2021"}
+                    logo={internlogo}
                   />
                 </motion.div>
               </motion.div>
             </>
           )}
         </div>
+
         {!isMobile && (
           <div className="experienceCardsContainer experienceCardsContainerFlex inMobile">
-            <motion.div className="dragConstraintBox" ref={constraintsRef3}>
-              <motion.div
-                drag
-                dragConstraints={constraintsRef3}
-                // dragElastic={0.2}
-                // style={{ width: "fit-content" }}
-              >
+            {/* Jio */}
+            <motion.div className="dragConstraintBox" ref={constraintsRef2}>
+              <motion.div drag dragConstraints={constraintsRef2}>
                 <ExperienceCards
-                  company={"eka.care"}
+                  company={"Jio Platforms Limited"}
                   role={"Software Development Engineer"}
-                  duration={"Sept 2025 - May 2026"}
-                  logo={ekalogo}
+                  duration={"July 2023 - Sept 2025"}
+                  logo={jiologo}
                 />
               </motion.div>
             </motion.div>
 
-            <motion.div className="dragConstraintBox" ref={constraintsRef4}>
-              <motion.div
-                drag
-                dragConstraints={constraintsRef4}
-                // dragElastic={0.2}
-                // style={{ width: "fit-content" }}
-              >
+            {/* SkillShip */}
+            <motion.div className="dragConstraintBox" ref={constraintsRef1}>
+              <motion.div drag dragConstraints={constraintsRef1}>
                 <ExperienceCards
-                  company={"Scapia Cards"}
-                  role={"Software Development Engineer"}
-                  duration={"May 2026 - Present"}
-                  logo={scapialogo}
+                  company={"SkillShip Foundation"}
+                  role={"Web Development Intern"}
+                  duration={"July 2020 - Jan 2021"}
+                  logo={internlogo}
                 />
               </motion.div>
             </motion.div>
